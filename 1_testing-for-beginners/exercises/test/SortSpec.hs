2456baf9526ea602b61f08a8399b441834fb4e74
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-06-20 10:41:19 +0200

    property test

--- 1_testing-for-beginners/exercises/test/SortSpec.hs
+++ 1_testing-for-beginners/exercises/test/SortSpec.hs
@@ -1,6 +1,7 @@
 module SortSpec where
 
 import Test.Hspec
+import Test.QuickCheck
 
 import Sort
 
@@ -10,3 +11,4 @@ spec =
     it "does nothing to an empty list" $ sor [] `shouldBe` ([] :: [Int])
     it "does nothing to a list of 5" $ sor [5] `shouldBe` [5]
     it "reverses this list correctly" $ sor [1, 5, 4] `shouldBe` [1, 4, 5]
+    it "is idempotent" $ property $ \xs -> sor (sor xs) `shouldBe` sor (xs :: [Int])
