9b8ca5476a053fc8ffd509c5c2df1f5dd3227645
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-06-20 12:55:07 +0200

    solutions

--- 3_overview-of-property-testing/exercises/src/Lib.hs
+++ 3_overview-of-property-testing/exercises/src/Lib.hs
@@ -1,12 +1,29 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Lib where
 
 import Hedgehog
-import Test.QuickCheck
+import Test.QuickCheck as Q
 
 data IntOrString
-  = I Int
+  = I Int -- I :: Int -> IntOrString
   | S String
   deriving (Show, Eq)
+
+-- elements :: [a] -> Gen a
+-- arbitrary :: Arbitrary a => Gen a
+-- oneof :: [Gen a] -> Gen a
+-- suchThat :: Gen a -> (a -> Bool) -> Gen a
+-- fmap  :: (a -> b) -> Gen a -> Gen b
+-- (<$>) :: (a -> b) -> Gen a -> Gen b
+instance Arbitrary IntOrString where
+  arbitrary :: Q.Gen IntOrString
+  arbitrary = oneof [I <$> arbitrary, S <$> arbitrary]
+  shrink :: IntOrString -> [IntOrString]
+  shrink ios =
+    case ios of
+      I i -> [I i' | i' <- shrink i] -- I <$> shrink i
+      S s -> I 0 : [S s' | s' <- shrink s]
 
 data MyType =
   MyType
@@ -15,7 +32,6 @@ data MyType =
     }
   deriving (Show, Eq)
 
-data Tree a
-  = Leaf a
-  | Branch (Tree a) (Tree a) (Tree a)
-  deriving (Show, Eq)
+instance Arbitrary MyType where
+  arbitrary = MyType <$> arbitrary <*> arbitrary
+  shrink (MyType b r) = [MyType b' r' | (b', r') <- shrink (b, r)]
